fc91e5ffe5ce993067df28afe4d583bef7822c94
Lucas Côrtes <lmattoscortes@gmail.com>
2021-06-06 16:11:05 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+
+<?php include_once 'head.php'; ?>
+
+<body>
+
+<div class='centro'>
+        <div class='container-top'>
+                <img src='tijolinhoportijolinho.png'></img>
+                <p class='amarelo'>
+                        as coisas estão mudando mas
+                        você pode <a href='https://open.spotify.com/artist/2cHZgbZDJncmSN7BEKOB4x' class='centro beige'>escutar no spotify</a> enquanto isso
+                </p>
+        </div>
+</div>
+<div class='container-bottom'>
+        <p class='centro amarelo'>
+                (e você pode até <a class='centro beige' href='mailto:lmattoscortes@gmail.com'>mandar um email</a> também, se você quiser falar alguma coisa)
+        </p>
+</div>
+
+</body>
+</html>
